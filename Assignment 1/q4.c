38c9cc97e853940bcd5f289e6cce937d0936ca1d
ayush-1801 <ayush23162@iiitd.ac.in>
2024-09-11 16:25:09 +0530

    Added README.md

--- Assignment 1/q4.c
+++ Assignment 1/q4.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Process{
     int pid, arrival_time, burst_time, response_time, turnaround_time;
 };
 
-void fifo(struct Process process[]){
-    return;
+int cmp(const void *p, const void *q){
+    const struct Process *proc1 = (const struct Process*)p;
+    const struct Process *proc2 = (const struct Process*)q;
+
+    return proc1->arrival_time - proc2->arrival_time;
+}
+
+void fifo(struct Process process[], int n){
+    qsort(process, n, sizeof(struct Process), cmp);
+
+    for(int i=0; i<n; i++){
+        printf("%d\n", process[i].pid);
+    }
 }
 
 void sjf(){
@@ -34,6 +46,6 @@ int main(){
     int time_quantum;
     scanf("%d", &time_quantum);
 
-    fifo(process);
+    fifo(process, n);
     return 0;
 }
